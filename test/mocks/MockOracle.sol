9229860dc5935233ca58ccfdb116efdf71f5bfec
Your Namediverminee <179622532+diverminee@users.noreply.github.com>
2026-02-27 04:06:45 +0700

    test: update MockOracle

--- test/mocks/MockOracle.sol
+++ test/mocks/MockOracle.sol
@@ -14,4 +14,14 @@ contract MockOracle is ITradeOracle {
     function verifyTradeData(bytes32 /*tradeDataHash*/ ) external view returns (bool) {
         return shouldVerify;
     }
+
+    function verifyTradeDataWithDocuments(
+        bytes32 /*tradeDataHash*/,
+        bytes32 /*invoiceHash*/,
+        bytes32 /*bolHash*/,
+        bytes32 /*packingHash*/,
+        bytes32 /*cooHash*/
+    ) external view returns (bool) {
+        return shouldVerify;
+    }
 }
